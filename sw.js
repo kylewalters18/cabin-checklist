0ce384ef6cb3742058e51fe6b635d6855668e390
Kyle Walters <kylewalters18@gmail.com>
2026-03-28 16:39:30 -0600

    cabin checklist

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll([
+      '/',
+      '/index.html',
+      '/manifest.json'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
